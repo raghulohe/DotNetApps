7f075fefb67e843795f8cc480606b184fb085ad4
raghu37 <raghulohe@gmail.com>
2019-05-30 21:25:32 -0400

    Initial Commit

---
+++ CountingKs/Models/ModelFactory.cs
@@ -0,0 +1,31 @@
+﻿using CountingKs.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountingKs.Models
+{
+    public class ModelFactory
+    {
+        public FoodModel Create(Food food)
+        {
+            return new FoodModel()
+            {
+                Description = food.Description,
+                Id = food.Id,
+                Measures = food.Measures.Select(m => Create(m)
+               )
+            };
+        }
+
+        public MeasureModel Create(Measure measure)
+        {
+            return new MeasureModel()
+            {
+                Description = measure.Description,
+                Calories = measure.Calories
+            };
+        }
+    }
+}
